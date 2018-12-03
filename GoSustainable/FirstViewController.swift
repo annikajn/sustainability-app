4b57f72ba029bb3b9b3f3f4a6bcdaecfd51760ea
annikanic <14ajn@queensu.ca>
2018-12-03 14:13:21 -0500

    Initial Commit

---
+++ GoSustainable/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GoSustainable
+//
+//  Created by Annika Nicol on 2018-12-03.
+//  Copyright © 2018 Ajane. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GoSustainable/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GoSustainable
+//
+//  Created by Annika Nicol on 2018-12-03.
+//  Copyright © 2018 Ajane. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
